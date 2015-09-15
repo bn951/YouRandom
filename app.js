e8868a8d91aafebdfeb641bdb8b54aec8b85f2fd
Bobby Nijjar <bobby951@gmail.com>
2015-09-15 15:13:30 -0400

    created category module

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require.config({
 	paths: {
 		"jquery": "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min",
 		"video": 'lib/modules/video',
-		"vidinfo": "lib/modules/vidinfo"
+		"vidinfo": "lib/modules/vidinfo",
+		"category": "lib/modules/category"
 	}
 });
 define(['jquery'], function($) {
